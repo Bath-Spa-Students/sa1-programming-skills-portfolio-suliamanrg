2f0de5752f73bbc6010260b959e8b42ebfc90fa4
suliamanrg <suliamanrehmat@gmail.com>
2024-10-16 14:39:39 +0400

    Create Coading is cool.py
    Coding is cool

---
+++ EXERCISE 01/Coading is cool.py
@@ -0,0 +1,11 @@
+#First word is assigned to the variable word1.
+word1 = "Coding"
+
+#The second word is assigned to the variable word2.
+word2 = "is"
+
+# Setting the variable word3 as the third word
+word3 = "Cool"
+
+# The final statement or output will be displayed in print form.
+print(word1 + " " + word2 + " " + word3)
