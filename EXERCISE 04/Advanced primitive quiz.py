635fa846b28a469e39032be9c5e0ac49eb5c533e
suliamanrg <suliamanrehmat@gmail.com>
2024-10-29 16:55:18 +0400

    advancedprimitivequiz.py
    Advanced Primitive Quiz
    name: Sulaiman Rehmat Gl Y1 G1

---
+++ EXERCISE 04/Advanced primitive quiz.py
@@ -0,0 +1,192 @@
+#Advanced primitive Quiz
+
+#in advanced primitive quiz we have to question the user for the capitals of 10 European countries.
+
+#Question 1
+answer= "paris"
+#answer is a variable which contains the correct answer for the question
+
+question = input ("1.What is the capital of France?: ")
+#question is a variable which will contain the answer given bt the user for the question asked
+
+#we will use lower() function so that the users answer is shown correct even if it has any upper or lower cases
+#for the result we will use if and else statement 
+
+if question.lower() == answer :
+    print ("Your answer is correct.")
+
+else:
+    print("Your answer is incorrect")
+
+#if the answer is given correctly the system will show your answer is correct otherwise it will show your answer is incorect    
+
+
+#Question 2
+answer= "berlin"
+#answer is a variable which contains the correct answer for the question
+
+question = input ("2.What is the capital of Germany?: ")
+#question is a variable which will contain the answer given bt the user for the question asked
+
+#we will use lower() function so that the users answer is shown correct even if it has any upper or lower cases
+#for the result we will use if and else statement 
+
+if question.lower() == answer :
+    print ("Your answer is correct.")
+
+else:
+    print("Your answer is incorrect")
+
+#if the answer is given correctly the system will show your answer is correct otherwise it will show your answer is incorect
+
+
+#Question 3
+answer= "vienna"
+#answer is a variable which contains the correct answer for the question
+
+question = input ("3.What is the capital of Austria?: ")
+#question is a variable which will contain the answer given bt the user for the question asked
+
+#we will use lower() function so that the users answer is shown correct even if it has any upper or lower cases
+#for the result we will use if and else statement 
+
+if question.lower() == answer :
+    print ("Your answer is correct.")
+
+else:
+    print("Your answer is incorrect")
+
+#if the answer is given correctly the system will show your answer is correct otherwise it will show your answer is incorect
+
+
+#Question 4
+answer= "rome"
+#answer is a variable which contains the correct answer for the question
+
+question = input ("4.What is the capital of Italy?: ")
+#question is a variable which will contain the answer given bt the user for the question asked
+
+#we will use lower() function so that the users answer is shown correct even if it has any upper or lower cases
+#for the result we will use if and else statement 
+
+if question.lower() == answer :
+    print ("Your answer is correct.")
+
+else:
+    print("Your answer is incorrect")
+
+#if the answer is given correctly the system will show your answer is correct otherwise it will show your answer is incorect
+
+
+#Question 5
+answer= "warsaw"
+#answer is a variable which contains the correct answer for the question
+
+question = input ("5.What is the capital of Poland?: ")
+#question is a variable which will contain the answer given bt the user for the question asked
+
+#we will use lower() function so that the users answer is shown correct even if it has any upper or lower cases
+#for the result we will use if and else statement 
+
+if question.lower() == answer :
+    print ("Your answer is correct.")
+
+else:
+    print("Your answer is incorrect")
+
+#if the answer is given correctly the system will show your answer is correct otherwise it will show your answer is incorect
+
+
+#Question 6
+answer= "madrid"
+#answer is a variable which contains the correct answer for the question
+
+question = input ("6.What is the capital of Spain?: ")
+#question is a variable which will contain the answer given bt the user for the question asked
+
+#we will use lower() function so that the users answer is shown correct even if it has any upper or lower cases
+#for the result we will use if and else statement 
+
+if question.lower() == answer :
+    print ("Your answer is correct.")
+
+else:
+    print("Your answer is incorrect")
+
+#if the answer is given correctly the system will show your answer is correct otherwise it will show your answer is incorect
+
+
+#Question 7
+answer= "athens"
+#answer is a variable which contains the correct answer for the question
+
+question = input ("7.What is the capital of Greece?: ")
+#question is a variable which will contain the answer given bt the user for the question asked
+
+#we will use lower() function so that the users answer is shown correct even if it has any upper or lower cases
+#for the result we will use if and else statement 
+
+if question.lower() == answer :
+    print ("Your answer is correct.")
+
+else:
+    print("Your answer is incorrect")
+
+#if the answer is given correctly the system will show your answer is correct otherwise it will show your answer is incorect
+
+
+#Question 8
+answer= "stockholm"
+#answer is a variable which contains the correct answer for the question
+
+question = input ("8.What is the capital of Sweden?: ")
+#question is a variable which will contain the answer given bt the user for the question asked
+
+#we will use lower() function so that the users answer is shown correct even if it has any upper or lower cases
+#for the result we will use if and else statement 
+
+if question.lower() == answer :
+    print ("Your answer is correct.")
+
+else:
+    print("Your answer is incorrect")
+
+#if the answer is given correctly the system will show your answer is correct otherwise it will show your answer is incorect
+
+
+#Question 9
+answer= "brussels"
+#answer is a variable which contains the correct answer for the question
+
+question = input ("9.What is the capital of Belgium?: ")
+#question is a variable which will contain the answer given bt the user for the question asked
+
+#we will use lower() function so that the users answer is shown correct even if it has any upper or lower cases
+#for the result we will use if and else statement 
+
+if question.lower() == answer :
+    print ("Your answer is correct.")
+
+else:
+    print("Your answer is incorrect")
+
+#if the answer is given correctly the system will show your answer is correct otherwise it will show your answer is incorect
+
+
+#Question 10
+answer= "london"
+#answer is a variable which contains the correct answer for the question
+
+question = input ("10.What is the capital of United Kingdom?: ")
+#question is a variable which will contain the answer given bt the user for the question asked
+
+#we will use lower() function so that the users answer is shown correct even if it has any upper or lower cases
+#for the result we will use if and else statement 
+
+if question.lower() == answer :
+    print ("Your answer is correct.")
+
+else:
+    print("Your answer is incorrect")
+
+#if the answer is given correctly the system will show your answer is correct otherwise it will show your answer is incorect
