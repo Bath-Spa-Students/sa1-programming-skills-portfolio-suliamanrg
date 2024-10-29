dde6e914b3616e2482db172868cc01d90e7e836a
suliamanrg <suliamanrehmat@gmail.com>
2024-10-29 16:54:20 +0400

    primitivequiz.py
    Primitive Quiz
    name: Sulaiman Rehmat Gul Y1 G1

---
+++ EXERCISE 04/Primitive quiz.py
@@ -0,0 +1,16 @@
+#primitive Quiz 1
+
+answer= "Paris"
+
+#answer is a variable which is used for the correct answer of the question
+
+question= input("what is the capital of France?:")
+
+#for the results we will use if else statement
+if question == answer :
+    print("Your answer is correct")
+
+else:
+    print("Your answer is incorrect")
+
+#if the answer is given correctly the system will show your answer is correct otherwise it will show your answer is incorect     
