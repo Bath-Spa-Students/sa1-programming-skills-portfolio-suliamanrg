c9d0d689d90af4bb5bd9bd8dd13b978921cfc143
suliamanrg <suliamanrehmat@gmail.com>
2024-11-21 14:47:40 +0400

    Simple sums.py

--- EXERCISE 02/simple sums.py
+++ EXERCISE 02/simple sums.py
@@ -1,17 +1,11 @@
-# Step 1:  Take a variable of value 10
-first_number = 10
+# Step 1:  Take a variable of value 8
+first_number = 8
 
-# Step 2: Take a second variable of value 15
-second_number = 15
+# Step 2: Take a second variable of value 10
+second_number = 10
 
-# Step 3: Take a third variable of value 20
-third_number = 20
+# Step 3: Calculate the sum of all the four numbers and save it in a new variable named as sum of numbers
+sum_of_numbers = first_number + second_number 
 
-# Step 4: Take a fourth variable of value 25
-forth_number = 25
-
-# Step 5: Calculate the sum of all the four numbers and save it in a new variable named as sum of numbers
-sum_of_numbers = first_number + second_number + third_number + forth_number
-
-# Step 6: Now command the system to give the output of the sum of numbers
-print("The sum of", first_number, second_number, third_number, "and", forth_number, "is:", sum_of_numbers)
+# Step 4: Now command the system to give the output of the sum of numbers
+print(sum_of_numbers)
